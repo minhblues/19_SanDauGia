99dd91849445187cc26823006a32cf7f02707954
MinhBLues <49396338+MinhBLues@users.noreply.github.com>
2020-01-05 02:25:01 +0700

    Thêm chức năng cập nhật thông tin cá nhân, thêm ngày sinh vào thông tin cá nhân khi đăng kí

--- Nodejs/models/users.model.js
+++ Nodejs/models/users.model.js
@@ -2,7 +2,6 @@ const db = require('../utils/db');
 
 module.exports = {
     all: () => db.load('select * from users'),
-    single: id => db.load(`select * from users where user_ID = ${id}`),
     singleByUserName: async username => {
         const rows = await db.load(`select * from users where Username = '${username}'`);
         if (rows.length === 0)
@@ -25,4 +24,9 @@ module.exports = {
                 return 0;
             else return ret[0].Score / ret[0].RateTime
     },
+    patch: entity => {
+        const condition = { Username: entity.Username };
+        delete entity.Username;
+        return db.patch('users', entity, condition);
+      },
 }
