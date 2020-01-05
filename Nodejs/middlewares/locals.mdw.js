adeab0051fb18551501ebdfe5a674a86a1d06d57
minhhddd <minhcine99@gmail.com>
2020-01-05 17:24:51 +0700

    Thêm quản lý danh sách người dùng

--- Nodejs/middlewares/locals.mdw.js
+++
@@ -1,8 +0,0 @@
-categoryModel = require('../models/category.model')
-
-module.exports = async(req, res, next) => {
-    if (!res.locals.lsCategories)
-        res.locals.lsCategories = await categoryModel.all();
-
-    next();
-}
