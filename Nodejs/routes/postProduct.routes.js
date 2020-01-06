7788e72f252e3bc889be07634669c53690c88d34
MinhBLues <49396338+MinhBLues@users.noreply.github.com>
2020-01-06 11:01:35 +0700

    sửa css, postproduct

--- Nodejs/routes/postProduct.routes.js
+++ Nodejs/routes/postProduct.routes.js
@@ -1,18 +1,17 @@
 var express = require('express');
+const multer = require('multer')
+
+const storage = multer.diskStorage({
+  filename: function (req, file, cb) {
+    cb(null, file.originalname)
+  },
+  destination: function (req, file, cb) {
+    cb(null, `./public/images/`);
+  },
+});
+const upload = multer({ storage });
+
 var router = express.Router();
-
-
-router.post('/', async (req, res) => {
-    console.log(req.body);
-  //  console.log(req.files);
-    const url = [];
-    for (const file of req.files) {
-        const result = await cloudinary.v2.uploader.upload(file.path);
-        url.push(result.url);
-    }
-    console.log(url);
-    res.send('ok');
-})
 
 
 router.get('/', function (req, res, next) {
@@ -20,5 +19,31 @@ router.get('/', function (req, res, next) {
         title: 'Đăng sản phẩm',
     });
 });
+
+router.post('/', function (req, res) {
+    console.log(req.body);
+    upload.single('fuMain')(req, res, err => {
+      if (err) { }
+  
+      res.send('ok');
+    });
+  })
+// router.post('/', async (req, res) => {
+//     console.log(req.body);
+//   //  console.log(req.files);
+//   upload.single('fuMain')(req, res, err => {
+//     if (err) { }
+
+//     res.send('ok');
+//   });
+//     // const url = [];
+//     // for (const file of req.files) {
+//     //     console.log(file);
+//     //     const result = await cloudinary.v2.uploader.upload(file.path);
+//     //     url.push(result.url);
+//     // }
+//     // console.log(url);
+//     // res.send('ok');
+// });
 
 module.exports = router;
